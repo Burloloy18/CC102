a5457e3e8a0687396d1b3d1eefb1233650c572e8
Burloloy18 <tenaclark781@gmail.com>
2024-10-08 09:33:59 +0800

    Create 03VariableDeclaration.cpp

---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int number;
+	
+	cout << "How old are you? " << endl;
+	cout << "Enter your age: ";
+	cin >> number;
+	cout << "Thank you! " << endl;
+	
+	return 0;
+}
