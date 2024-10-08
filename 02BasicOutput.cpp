e50ef1a661df6fe67ec66fc1773e12d8f7525653
Burloloy18 <tenaclark781@gmail.com>
2024-10-08 09:32:52 +0800

    Create 02BasicOutput.cpp

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our lady of lourdes colleege foundation" << endl;
+	cout << "CCS Department" << endl;
+	cout << "BSIT First Year Block A" << endl;
+  cout << "Clark Jedric I. Teña" << endl;
+	cout << "Purok-1 BInanuaan, Talisay, Camarines Norte" << endl;
+return 0;
+}
