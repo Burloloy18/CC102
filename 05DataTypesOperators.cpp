26cfc420d56c7121e366c6fbb14cb71d10e342fa
Burloloy18 <tenaclark781@gmail.com>
2024-10-09 09:26:52 +0800

    Update 05DataTypesOperators.cpp

--- 05DataTypesOperators.cpp
+++ 05DataTypesOperators.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 int main() {
-    float num1, num2;
+    double num1, num2;
 
     cout << "Enter the first number: ";
     cin >> num1;
     cout << "Enter the second number: ";
     cin >> num2;
 
-    float sum = num1 + num2;
-    float difference = num1 - num2;
-    float product = num1 * num2;
-    float quotient = (num2 != 0) ? num1 / num2 : 0; 
+    double sum = num1 + num2;
+    double difference = num1 - num2;
+    double product = num1 * num2;
+    double quotient = (num2 != 0) ? num1 / num2 : 0; 
     int remainder = (num2 != 0) ? (num1) % (num2) : 0; 
 	
     cout << "Sum: " << sum << endl;
