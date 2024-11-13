38d544430a0641715598f8ad3fc139b54551ca90
Burloloy18 <tenaclark781@gmail.com>
2024-11-13 09:17:16 +0800

    Create Temperature.cpp

---
+++ Temperature.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+ using namespace std; 
+ 
+int main () {
+	  int temperature; 
+	    cout << "Enter the temperature: ";
+	    cin  >> temperature;
+	  if (temperature > 32); { 
+	        cout << "Bring heavy jacket." << endl; 
+    } if else (temperature  >= 32 && temperature <= 50) {
+	        cout << "Bring a light jakcet." << endl;
+    } else  {  (temperature <= 50);
+	        cout << "Do not bring any jacket." << endl; 
+    } 
+	 return 0;
+}	 
