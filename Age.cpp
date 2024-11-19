e6144e5f6de4a60d0ea058bad16289bdd037931c
Burloloy18 <tenaclark781@gmail.com>
2024-11-19 09:12:30 +0800

    Age.cpp

--- Age.cpp
+++ Age.cpp
@@ -10,8 +10,8 @@ int main () {
 	      cout << "Too Young to Drive" << endl; 
      } else if (age == 16) {
 		    cout << "Better Clear the road" << endl;
-	   } else if (age > 16 & age < 65) {
+     } else if (age > 16 ) {
 		    cout << "You are getting pretty old" << endl;
-	   }
+     }
 	   return 0;
 }
